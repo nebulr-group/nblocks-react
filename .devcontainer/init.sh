920d8dfcc94c617af4785410cfc7186bba515009
Nemanja Manojlovic <nemanja@nebulr.group>
2022-10-16 18:02:18 +0200

    Create init shell scripts and add them to devcontainer config as post create command

---
+++ .devcontainer/init.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+## Usage
+## ./init_local.sh WORKSPACE_FOLDER_PATH
+PWD=$(dirname $0)
+source $PWD/secrets.env
+
+echo "Installing utils using apt..."
+apt-get update
+apt-get -y install vim less jq curl
+
+echo "Configuring GIT"
+git config --global user.email $GIT_EMAIL
+git config --global user.name $GIT_NAME
+
+# echo "Installing global dependencies..."
+
+echo "Done dev specific initializing, running workspace_folder/init.sh..."
+. $1/init.sh
---
+++ init.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+## This file setups all prerequisites for the docker container, used both dev environment and in CI/CD pipeline.
+## For prerequisites strictly related to dev environment see ./.devcontainer/init.sh which is run before this script.
+echo "Done initializing"
