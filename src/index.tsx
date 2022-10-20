f03851a9d12f92bb3db36baf1dccca1548e8c499
Nemanja Manojlovic <nemanja@nebulr.group>
2022-10-20 22:07:49 +0200

    Create NblocksButton component

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,14 @@ import { useAuth } from "./hooks/auth-context";
 import { NblocksProvider } from "./providers/NblocksProvider/NblocksProvider";
 import { useApp } from "./hooks/app-context";
 import { SetupRoutes } from "./routes/SetupRoutes";
+import { NblocksButton } from "./components/shared/NblocksButton";
+import "./index.css";
 
 export {
-    AuthRoutes, SetupRoutes, useAuth, useApp, NblocksProvider
-}
+  AuthRoutes,
+  SetupRoutes,
+  useAuth,
+  useApp,
+  NblocksProvider,
+  NblocksButton,
+};
