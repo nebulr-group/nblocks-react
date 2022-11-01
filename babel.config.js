fc056483d66c2b291e35d463007fd691c174a797
Nemanja Manojlovic <nemanja@nebulr.group>
2022-11-01 15:07:11 +0100

    Update babel.config.js with preset settings

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,23 @@
+const pak = require("./package.json");
+
 module.exports = {
-  presets: ["@babel/preset-typescript", "@babel/preset-typescript"],
+  presets: [
+    [
+      "@babel/preset-env",
+      {
+        modules: "auto",
+        targets: {
+          chrome: "107",
+          edge: "15",
+          safari: "10",
+          firefox: "54",
+          opera: "64",
+          samsung: "5",
+          android: "13.4",
+        },
+      },
+    ],
+    "@babel/preset-react",
+    "@babel/preset-typescript",
+  ],
 };
