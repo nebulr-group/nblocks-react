4bef6f477dd1b05ec6ae0516cc8fe65aa7633f56
Oscar Söderlund <oscar@nebulr.group>
2022-11-04 17:13:10 +0100

    Beta release with a new component InstallationCompleteComponent

--- src/index.tsx
+++ src/index.tsx
@@ -24,6 +24,7 @@ import { UserListTableComponent } from "./components/shared/UserListTableCompone
 import { ChipComponent } from "./components/shared/ChipComponent";
 import "./index.css";
 import { AlertComponent } from "./components/shared/AlertComponent";
+import { InstallationCompleteComponent } from "./components/shared/InstallationCompleteComponent";
 
 export {
   useConfig,
@@ -50,6 +51,7 @@ export {
   UserListTableComponent,
   ChipComponent,
   AlertComponent,
+  InstallationCompleteComponent,
 };
 
 export type { LibConfig };
