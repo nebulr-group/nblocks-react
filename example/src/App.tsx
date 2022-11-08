2035594ae417866574a19f91516f6b354fc1c4c1
Oscar Söderlund <oscar@nebulr.group>
2022-11-08 17:56:48 +0100

    Bump graphql declarations. Requires  @nebulr-group/nblocks-nestjs@1.3.7-beta.3

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -6,20 +6,7 @@ function App() {
   return (
     <div className="App">
       <NblocksProvider config={{ debug: true, devMode: true }}>
-        <header className="App-header">
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          <InstallationCompleteComponent />
-        </header>
+        <InstallationCompleteComponent />
       </NblocksProvider>
     </div>
   );
