02c8ed5827b015415066af9053565efecea0ce47
Nemanja Manojlovic <nemanja@nebulr.group>
2022-11-14 09:53:07 +0100

    Add getCurrencySymbol utility

--- src/utils/ComponentHelpers.ts
+++ src/utils/ComponentHelpers.ts
@@ -2,5 +2,21 @@ const classNameFilter = (...classes: any[]) => {
   return classes.filter(Boolean).join(" ");
 };
 
+// ISO 4217 => symbol
+const getCurrencySymbol = (currency: string) => {
+  switch (currency) {
+    case "USD":
+      return "$";
+    case "GPB":
+      return "£";
+    case "EUR":
+      return "€";
+    case "JPY":
+      return "¥";
+    default:
+      return currency;
+  }
+};
 
-export { className };
+
+export { classNameFilter, getCurrencySymbol };
