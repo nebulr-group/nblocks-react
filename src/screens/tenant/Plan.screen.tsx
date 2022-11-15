18bb56460a3d9f5cf45d30edab01ebcf5a6decd5
Nemanja Manojlovic <nemanja@nebulr.group>
2022-11-15 22:56:46 +0100

    Update choosing a plan logic

--- src/screens/tenant/Plan.screen.tsx
+++ src/screens/tenant/Plan.screen.tsx
@@ -1,9 +1,31 @@
-import React, { FunctionComponent } from "react";
+import { useQuery } from "@apollo/client";
+import React, { FunctionComponent, useContext, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import { ChoosePlanComponent } from "../../components/tenant/plan/ChoosePlanComponent";
 import { TenantLayoutWrapperComponent } from "../../components/tenant/TenantLayoutWrapperComponent";
+import { GetTenantDocument, GetTenantQuery } from "../../gql/graphql";
+import { useConfig } from "../../hooks/config-context";
+import { RouteConfig } from "../../routes/AuthRoutes";
+
 // import { TabsComponent } from "../../components/shared/TabsComponent";
 
 const PlanScreen: FunctionComponent<{}> = () => {
+  const navigate = useNavigate();
+  const { handoverRoute } = useConfig();
+  const { data, loading, error } = useQuery(GetTenantDocument);
+  const planSelectHandler = () => {
+    if (data) {
+      const paymentsRequired = data.getTenant.paymentsRequired;
+
+      switch (paymentsRequired) {
+        case false:
+          return navigate(handoverRoute);
+        case true:
+          return navigate(RouteConfig.tenant.payment);
+      }
+    }
+  };
+
   return (
     <TenantLayoutWrapperComponent
       heading={"Pricing plans"}
@@ -11,7 +33,7 @@ const PlanScreen: FunctionComponent<{}> = () => {
         "We belive Nblocks should be available to all companies, no matter the size."
       }
     >
-      <ChoosePlanComponent />
+      <ChoosePlanComponent planSelectHandler={planSelectHandler} />
     </TenantLayoutWrapperComponent>
   );
 };
