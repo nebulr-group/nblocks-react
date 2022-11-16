bd475038a75db24282f0a747be3759ff81136188
Nemanja Manojlovic <nemanja@nebulr.group>
2022-11-16 14:46:26 +0100

    Update config page style and add tanstack table for rendering plans data

--- src/components/setup/ConfigComponent.tsx
+++ src/components/setup/ConfigComponent.tsx
@@ -1,6 +1,10 @@
 import { useMutation, useQuery } from "@apollo/client";
 import React, { FunctionComponent, useEffect, useState } from "react";
-import { GetAppConfigDocument } from "../../gql/graphql";
+import {
+  GetAppConfigDocument,
+  PlanGraphql,
+  PriceGraphql,
+} from "../../gql/graphql";
 import { HeadingComponent } from "../shared/HeadingComponent";
 import { ModalComponent } from "../shared/ModalComponent";
 import { NblocksButton } from "../shared/NblocksButton";
@@ -10,6 +14,15 @@ import { InputComponent } from "../shared/InputComponent";
 import { UpdateAppCredentialsDocument } from "../../gql/graphql";
 import { AlertComponent, ComponentProps } from "../shared/AlertComponent";
 import { SkeletonLoader } from "../shared/SkeletonLoader";
+import {
+  createColumnHelper,
+  flexRender,
+  getCoreRowModel,
+  Row,
+  useReactTable,
+} from "@tanstack/react-table";
+import { RouteConfig } from "../../routes/AuthRoutes";
+import { LinkComponent } from "../shared/LinkComponent";
 
 const ConfigComponent: FunctionComponent<{}> = ({}) => {
   const [stripeFormIsValid, setStripeFormIsValid] = useState<boolean>(false);
@@ -24,7 +37,9 @@ const ConfigComponent: FunctionComponent<{}> = ({}) => {
     },
     show: false,
   });
-
+  const [credentialsModalOpen, setCredentialsModalOpen] = useState(false);
+  const [stripeSecretKey, setStripePrivateKey] = useState<string>("");
+  const [stripePublicKey, setStripePublicKey] = useState<string>("");
   useEffect(() => {
     if (alert.show) {
       setTimeout(() => {
@@ -34,14 +49,120 @@ const ConfigComponent: FunctionComponent<{}> = ({}) => {
       }, 5000);
     }
   }, [alert.show]);
-
   const { data, loading, error } = useQuery(GetAppConfigDocument);
   const [updateCredentialsMutation, updateAppCredentialsData] = useMutation(
     UpdateAppCredentialsDocument
   );
-  const [credentialsModalOpen, setCredentialsModalOpen] = useState(false);
-  const [stripeSecretKey, setStripePrivateKey] = useState<string>("");
-  const [stripePublicKey, setStripePublicKey] = useState<string>("");
+
+  const columnHelper = createColumnHelper<PlanGraphql>();
+
+  const buildCloudLink = (row: Row<PlanGraphql>) => {
+    const planName = row.getValue<string>("name");
+    const regionName = row.getValue<PriceGraphql[]>("region")[0].region;
+    return `https://account-api-stage.nebulr-core.com/app/${data?.getAppConfig.id}/checkoutView?plan=${planName}&region=${regionName}`;
+  };
+
+  const buildLocalLink = (row: Row<PlanGraphql>) => {
+    const appUiURL = data?.getAppConfig.uiUrl;
+    const signupRoute = RouteConfig.setup.signup;
+    return `${appUiURL}${signupRoute}/${row.getValue("name")}`;
+  };
+
+  const columns = [
+    columnHelper.accessor("name", {
+      header: "Plan Name",
+      cell: (info) => info.getValue(),
+    }),
+    columnHelper.group({
+      header: "Prices",
+
+      columns: [
+        columnHelper.accessor("prices", {
+          header: "Amount",
+          id: "amount",
+          cell: (info) => {
+            console.log(info.cell.getValue());
+            return info.cell.getValue<PriceGraphql[]>().map((price) => {
+              return (
+                <tr>
+                  <td>{price.amount}</td>
+                </tr>
+              );
+            });
+          },
+        }),
+        columnHelper.accessor("prices", {
+          header: "Currency",
+          id: "currency",
+          cell: (info) => {
+            return info.cell.getValue<PriceGraphql[]>().map((price) => {
+              return (
+                <tr>
+                  <td>{price.currency}</td>
+                </tr>
+              );
+            });
+          },
+        }),
+        columnHelper.accessor("prices", {
+          header: "Region",
+          id: "region",
+          cell: (info) => {
+            return info.cell.getValue<PriceGraphql[]>().map((price) => {
+              return (
+                <tr>
+                  <td>{price.region}</td>
+                </tr>
+              );
+            });
+          },
+        }),
+      ],
+    }),
+    columnHelper.group({
+      header: "Quick Links",
+      columns: [
+        columnHelper.display({
+          header: "In-app Plan Sign Up",
+          id: "in-app-link",
+          cell: (info) => {
+            return (
+              <LinkComponent to={buildLocalLink(info.row)} type={"primary"}>
+                {buildLocalLink(info.row)}
+              </LinkComponent>
+            );
+          },
+        }),
+        columnHelper.display({
+          header: "Nblocks Cloud Plan Sign Up",
+          id: "nblocks-cloud-link",
+          cell: (info) => {
+            return (
+              <LinkComponent
+                to={buildCloudLink(info.row)}
+                type={"primary"}
+                nativeBehavior={true}
+                target={"_blank"}
+                className={"block text-center"}
+              >
+                Link
+              </LinkComponent>
+            );
+          },
+        }),
+      ],
+    }),
+  ];
+
+  let planData = data?.getAppConfig
+    ? data.getAppConfig.businessModel?.plans
+    : [];
+
+  const table = useReactTable({
+    data: planData ? planData : [],
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+  });
 
   const onSubmitStripeCredentialsHandler: React.FormEventHandler<
     HTMLFormElement
@@ -100,14 +221,21 @@ const ConfigComponent: FunctionComponent<{}> = ({}) => {
           )}
         </div>
         <hr />
-        <div>
-          <HeadingComponent type={"h2"} size="xl">
+        <div className="bg-white w-80 border rounded-tr-md rounded-tl-md">
+          <HeadingComponent
+            type={"h2"}
+            size="xl"
+            className="bg-gray-50 p-4 border-b"
+          >
             User roles
           </HeadingComponent>
           {data ? (
-            <ul className="list-none inline-block mt-2">
+            <ul className="list-none inline-block mt-2 w-full">
               {data?.getAppConfig.roles?.map((role, index) => (
-                <li className="py-3" key={index}>
+                <li
+                  className="py-3 px-4 border-b last:border-b-0 block"
+                  key={index}
+                >
                   {role}{" "}
                   {role === data.getAppConfig.defaultRole ? "(Default)" : ""}
                 </li>
@@ -122,14 +250,67 @@ const ConfigComponent: FunctionComponent<{}> = ({}) => {
           )}
         </div>
         <hr />
-        <div>
-          <HeadingComponent type={"h2"} size="xl">
-            Plans
-          </HeadingComponent>
-          {data ? (
+        <div className="bg-white">
+          <div className="w-fit">
+            <HeadingComponent
+              type={"h2"}
+              size="xl"
+              className="bg-gray-50 p-4 border-t border-l border-r rounded-tr-md rounded-tl-md"
+            >
+              Plans
+            </HeadingComponent>
+
+            <table className="table-auto border">
+              <thead>
+                {table.getHeaderGroups().map((headerGroup) => {
+                  return (
+                    <tr key={headerGroup.id}>
+                      {headerGroup.headers.map((header) => (
+                        <th
+                          key={header.id}
+                          colSpan={header.colSpan}
+                          className={"border p-4"}
+                        >
+                          {header.isPlaceholder
+                            ? null
+                            : flexRender(
+                                header.column.columnDef.header,
+                                header.getContext()
+                              )}
+                        </th>
+                      ))}
+                    </tr>
+                  );
+                })}
+              </thead>
+              <tbody className="bg-white">
+                {table.getRowModel().rows.map((row) => {
+                  return (
+                    <tr key={row.id}>
+                      {row.getVisibleCells().map((cell) => {
+                        return (
+                          <td key={cell.id} className={"border-b p-4 pl-8"}>
+                            {flexRender(
+                              cell.column.columnDef.cell,
+                              cell.getContext()
+                            )}
+                          </td>
+                        );
+                      })}
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </table>
+          </div>
+
+          {/* {data ? (
             <ul className="list-none mt-2">
               {data?.getAppConfig.businessModel?.plans.map((plan, i) => (
-                <li key={i}>
+                <li
+                  key={i}
+                  className="py-3 px-4 border-b last:border-b-0 block"
+                >
                   {plan.name} -{" "}
                   {plan.prices.map((price, j) => (
                     <i key={j}>
@@ -145,7 +326,7 @@ const ConfigComponent: FunctionComponent<{}> = ({}) => {
               <SkeletonLoader className="h-6 w-40 rounded-lg mt-2" />
               <SkeletonLoader className="h-6 w-40 rounded-lg mt-2" />
             </>
-          )}
+          )} */}
         </div>
       </div>
 
