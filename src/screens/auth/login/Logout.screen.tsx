d5d255d669e0a7e3c04aa3018619410ea33f8f0c
Oscar Söderlund <oscar@nebulr.group>
2024-04-08 19:33:06 +0200

    Improved logout

--- src/screens/auth/login/Logout.screen.tsx
+++ src/screens/auth/login/Logout.screen.tsx
@@ -1,23 +1,19 @@
-import React, { useEffect, useState } from "react";
-import { Navigate } from "react-router-dom";
+import React, { useEffect } from "react";
 import { useAuth } from "../../../hooks/auth-context";
 import { useSecureContext } from "../../../hooks/secure-http-context";
 
 export function LogoutScreen() {
   const { logout } = useAuth();
   const { authService } = useSecureContext();
-  const [didLogout, setDidLogout] = useState(false);
 
   useEffect(() => {
     doLogout();
   }, []);
 
   const doLogout = async () => {
-    await logout();
-    setDidLogout(true);
+    await logout(true);
+    window.location.replace(authService.getLogoutUrl());
   };
 
-  if (didLogout)
-    return <Navigate to={authService.getLogoutUrl()} replace={true} />;
-  else return null;
+  return null;
 }
