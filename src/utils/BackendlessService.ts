9fc38ad65d204fbbc90fb8af7d45579b572cadad
Oscar Söderlund <oscar@nebulr.group>
2024-07-07 19:01:13 +0200

    Now it is working

--- src/utils/BackendlessService.ts
+++ src/utils/BackendlessService.ts
@@ -1,6 +1,5 @@
 import { AxiosInstance } from "axios";
 import { LibConfig } from "../models/lib-config";
-import { NblocksStorage } from "./Storage";
 
 export class BackendlessService {
   private readonly ENDPOINTS = {
@@ -18,26 +17,13 @@ export class BackendlessService {
     this.apiHost = config.apiHost;
   }
 
-  // If we have a custom redirect uri it will be consumed and removed when generating the URL
   async handoverToAppUrl(): Promise<string> {
     await this._setSecureCookie();
-    // Handle custom redirects flow
-    const redirectUri = BackendlessService._consumeCustomRedirectUri();
-    return `${this.apiHost}${this.ENDPOINTS.handoverToApp}${redirectUri ? `/?redirectUri=${redirectUri}` : ""}`;
+    return `${this.apiHost}${this.ENDPOINTS.handoverToApp}`;
   }
 
   private async _setSecureCookie(): Promise<void> {
     await this.httpClient.post(`${this.apiHost}${this.ENDPOINTS.setCookie}`, {},
       { baseURL: this.apiHost, withCredentials: true });
-  }
-
-  static setCustomRedirectUri(redirectUri: string) {
-    NblocksStorage.setItem('CUSTOM_REDIRECT', redirectUri);
-  }
-
-  private static _consumeCustomRedirectUri() {
-    const redirectUri = NblocksStorage.getItem('CUSTOM_REDIRECT');
-    NblocksStorage.removeItem('CUSTOM_REDIRECT');
-    return redirectUri;
   }
 }
